f17fc6cba3589f060c80830168737c646f21e475
Ravi Suhag <suhag.ravi@gmail.com>
2016-12-02 20:52:06 +0530

    Update life lessons

--- app/index.js
+++ app/index.js
@@ -11,7 +11,6 @@ google.maps.event.addDomListener(window, 'load', function() {
     var infowindow = new google.maps.InfoWindow();
 
     Lessons.forEach(function(lesson, index) {
-
         var r = 300 / 111300, // = 100 meters
             y0 = 50.9683611,
             x0 = 1.9059048,
@@ -33,9 +32,25 @@ google.maps.event.addDomListener(window, 'load', function() {
         //         marker_id: index
         //     }
         // );
+
+        var pinColor = 'E9D460';
+        if(lesson['Type'] === 'volunteer'){
+            pinColor = '1581C9';
+        }
+        var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor,
+            new google.maps.Size(21, 34),
+            new google.maps.Point(0, 0),
+            new google.maps.Point(10, 34));
+        var pinShadow = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_shadow",
+            new google.maps.Size(40, 37),
+            new google.maps.Point(0, 0),
+            new google.maps.Point(12, 35));
+
         var marker = new google.maps.Marker({
             position: new google.maps.LatLng(newY, newX),
-            map: map
+            map: map,
+            icon: pinImage,
+            shadow: pinShadow
         });
 
         marker.addListener('click', function() {
@@ -45,11 +60,11 @@ google.maps.event.addDomListener(window, 'load', function() {
             // document.getElementById('lesson-story').textContent = lesson['Story Behind the lesson'];
             // var img = Math.floor(Math.random() * 23) + 1;
             // document.getElementById('lesson-banner').style.backgroundImage = 'url(build/images/pictures/' + img + '.jpg)';
-            
+
             var img = Math.floor(Math.random() * 23) + 1;
             var contentString =
                 '<div id="lesson-banner" class="banner" style="background-image: url(build/images/pictures/' + img + '.jpg);"></div>' +
-                '<div>'+
+                '<div>' +
                 '<div id="lesson-title" class="title">' +
                 lesson['Life Lesson'] +
                 '</div>' +
@@ -57,7 +72,7 @@ google.maps.event.addDomListener(window, 'load', function() {
                 lesson['Story Behind the lesson'] +
                 '</p>' +
                 '<p id="lesson-author" class="u-text-right lesson-author">' +
-                '- ' + lesson['Name'] +', ' + lesson['Age'] + ' (' +  lesson['Country of Origin'] +')' +
+                '- ' + lesson['Name'] + ', ' + lesson['Age'] + ' (' + lesson['Country of Origin'] + ')' +
                 '</p>' +
                 '</div>';
 
